7b8e84f7d1995aa6563624fcf69dbdd4607d5c6d
Hoàng Minh Chính <mchinhlya@gmail.com>
2023-11-03 01:51:32 +0700

    add swagger(OpenAPI)

--- src/main/java/com/example/mystory/MyStoryApplication.java
+++ src/main/java/com/example/mystory/MyStoryApplication.java
@@ -2,8 +2,10 @@ package com.example.mystory;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
+@EnableWebSecurity
 public class MyStoryApplication {
     public static void main(String[] args) {
         SpringApplication.run(MyStoryApplication.class, args);
