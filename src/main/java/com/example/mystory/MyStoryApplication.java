3e6b7826140e8d3140298c23b650a5f7dacde9ff
Hoàng Minh Chính <mchinhlya@gmail.com>
2023-11-03 00:18:34 +0700

    rename main class and add css

--- src/main/java/com/example/mystory/MyStoryApplication.java
+++ src/main/java/com/example/mystory/MyStoryApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MystoryApplication {
+public class MyStoryApplication {
     public static void main(String[] args) {
-        SpringApplication.run(MystoryApplication.class, args);
+        SpringApplication.run(MyStoryApplication.class, args);
     }
 }
